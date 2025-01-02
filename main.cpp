13ebcf4b6cce489303e3bde8bf95c8218c951bcc
Ruba Osman <rubaosman10@gmail.com>
2025-01-01 21:25:54 -0600

    Created mvp app UI

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+<<<<<<< HEAD
 
+=======
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setMinimumSize(600, 480); // Min window size
+    w.setWindowIcon(QIcon(":/icons/icons/appLogo.png")); // set window/app logo
+
+    w.show();
+    return a.exec();
+}
+>>>>>>> b9c94abe14461709980ebd09e99edc718355f5e7
